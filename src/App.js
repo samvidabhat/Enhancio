4ca10cf37776f6b5e453e74d84fad35ef4e252ed
samvida <samvidabhat@gmail.com>
2020-01-11 01:02:25 +0530

    Enhancing code, clean up, implementing stars beside username

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import axios from 'axios';
-import "bootstrap/dist/css/bootstrap.min.css";
+import styled from 'styled-components';
 import {setServiceResponse} from './actions/actions.js';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import "./App.css"
-import {TiLocationOutline} from 'react-icons/ti';
+import {TiLocationOutline,TiStarFullOutline} from 'react-icons/ti';
 import {AiOutlineMail} from 'react-icons/ai';
 
+const Container=styled.div`
+  font-family:'Segoe UI',
+  padding-right: 15px;
+  padding-left: 15px;
+  margin-right: auto;
+  margin-left: auto;
+  
+`;
+
+const UserLink = props =>{
+  return <a {...props}>
+    {props.text}
+  </a>
+}
+const StyledAnchor = styled(UserLink)`
+  color: blue;
+  text-decoration: none;
+`;
 export const mapStateToProps = (state) =>{
   return {
   githubResp: state.githubrepos
+  
   }
 }
 export const mapDispatchToProps = (dispatch) =>{
@@ -25,59 +43,77 @@ export const mapDispatchToProps = (dispatch) =>{
   }
   
   async componentDidMount(){
-    let respData =[];
-    axios.get("https://api.github.com/search/repositories?q=location:Bangalore").then(resp=>{
-     
-      
-      resp.data.items.slice(0,10).map(async res=>{
+    let respData =[],detailsPromises=[];
+    axios.get("https://api.github.com/search/repositories?q=location:Bangalore&sort=stars").then(resp=>{
+      resp.data.items.slice(0,10).map(res=>{
         let userObj={
           owner_id:0,
           owner_name:"",
+          full_name:"",
           repoName:"",
           avatar_url:"",
           user_url:"",
           company:"",
           email:"test@gmail.com",
-          location:"Bangalore, India"
+          location:"Bangalore, India",
+          bio:"",
+          stars:res.watchers
         }  
         userObj.owner_id=res.owner.id
         userObj.repoName=res.name;
         userObj.owner_name=res.owner.login;
         userObj.avatar_url=res.owner.avatar_url;
-        userObj.user_url=res.owner.html_url
-        // await axios.get(res.owner.url).then(details=>{
-        //   console.log("details",details)
-        //   userObj.company = details.data.company !== null? details.company : "";
-        //   userObj.email=details.data.email !== null ? details.email : "";
-        // });
-        
+        userObj.user_url=res.owner.html_url;
+        detailsPromises.push(axios.get(res.owner.url));
         respData.push(userObj);
       });
-      console.log("respData",respData)
-      respData.length && this.props.setServiceResponse(respData);
-    });
-    // axios.get("https://api.github.com/search/repositories?q=location:Bangalore").then(resp=>{
-
-    // });
+      Promise.all(detailsPromises).then(details=>{
+        console.log("details",details)
+        details.forEach(item=>{
+          let index=respData.findIndex(data=>data.owner_id===item.data.id);
+          console.log();
+          respData[index].company = item.data.company !== null? item.data.company : "";
+          respData[index].email=item.data.email !== null ? item.data.email : "test@gmail.com";
+          respData[index].full_name=item.data.name;
+          respData[index].bio=item.data.bio;
+        });
+       
+        this.props.setServiceResponse(respData);
+      });
+      });
+  }
+  getWatchers(stars){
+    let starSpan=[];
+    for(let i=0;i<stars;i++){
+       starSpan.push(<span key={i} style={{marginLeft:5,marginTop:2,color:'gold'}}><TiStarFullOutline /></span>);
+    }
+    return starSpan;
+   
   }
   getBody(){
-    let data=this.props.githubResp &&   this.props.githubResp.map((repo,i)=>{
-      return <div key={'repo_'+i} style={{marginLeft:50}}>
-          <img width='50' className="spacing__vertical" src={repo.avatar_url} alt={repo.repoName} />
+     let data=this.props.githubResp &&   this.props.githubResp.map((repo,i)=>{
+      return <div key={'repo_'+i} style={{marginLeft:50,marginTop:20}}>
+          <img width='45' className="" src={repo.avatar_url} alt={repo.owner_name} />
           <div style={{display:'inline-block',marginLeft:10}}>
-          <a style={{fontSize:'1.25em'}} target="_blank" rel="noopener noreferrer" href={repo.user_url}> 
-          {repo.repoName}
-          </a><span style={{display:'inline-block',marginLeft:10, fontSize:'1.25em'}}>{repo.owner_name}</span>
-          <div><TiLocationOutline /> {repo.location} <AiOutlineMail />{repo.email} </div>
+          <StyledAnchor
+          target="_blank"
+           rel="noopener noreferrer"
+           text={repo.owner_name}
+          href={repo.user_url} />
+          <span style={{display:'inline-block',marginLeft:10, fontSize:'1em'}}>{repo.repoName}</span>
+          {this.getWatchers(repo.stars)}
+          <div style={{marginTop:5,marginBottom:5}}>{repo.bio}</div>
+          <div><TiLocationOutline /> {repo.location} <span style={{marginLeft:5}}><AiOutlineMail /> {repo.email} </span></div>
         </div>
       </div>;
     });
     return data;
   }
   render(){
-    console.log("githubResp",this.props.githubResp)
-    return  <div className="container" style={{marginTop:50}}>
-      {this.getBody()}
+     return  <div style={{marginTop:50}}>
+        <Container>
+        {this.getBody()}
+        </Container>
   </div>;
   }
 }
